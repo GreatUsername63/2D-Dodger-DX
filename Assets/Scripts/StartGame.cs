f833cb5a056cbe4e0efe8c9b64fb78c14ae4d05c
GreatUsername63 <RamirezSantiago244@gmail.com>
2024-12-08 00:10:55 -0700

    el que no chambea secret

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -8,19 +8,48 @@ public class StartGame : MonoBehaviour
 
     public Animator transition;
 
+    public GameObject secret;
+    public string secretString = "miprimerachamba";
+    public int secretIndex = 0;
+
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space)){
+        if (Input.GetKey(KeyCode.Space))
+        {
             StartCoroutine(LoadGame());
         }
+        DetectSecretString();
     }
 
-    IEnumerator LoadGame(){
-        transition.SetBool("Start",true);
+    IEnumerator LoadGame()
+    {
+        transition.SetBool("Start", true);
 
         yield return new WaitForSeconds(0.5f);
 
         SceneManager.LoadScene("Mygame");
+    }
+
+    void DetectSecretString()
+    {
+        string currentKey = secretString[secretIndex].ToString();
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown(currentKey))
+            {
+                secretIndex++;
+                Debug.Log(currentKey);
+                if (secretIndex >= secretString.Length)
+                {
+                    secret.SetActive(true);
+                    secretIndex = 0;
+                }
+            }
+            else
+            {
+                secretIndex = 0;
+            }
+        }
     }
 }
